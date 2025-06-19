6fc315fd8d4c8ed6ee8b5db5b50223f7ebedde5d
O-Tavares <114822357+O-Tavares@users.noreply.github.com>
2025-06-19 14:34:31 -0300

    fds

---
+++ style.css
@@ -0,0 +1,26 @@
+body {
+    background-color: #111;
+    color: #eee;
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+canvas {
+    background-color: #000;
+    border: 2px solid #fff;
+}
+
+h1 {
+    margin-bottom: 1rem;
+}
+
+p {
+    color: #aaa;
+    margin-top: 1rem;
+}
